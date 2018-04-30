8958cdda07762d641a1f41f305a649ef025287ad
yutaro ohira <detkid@gmail.com>
2018-05-01 00:35:50 +0900

    歌手ページから歌詞ページのURL一覧を取得

--- scraping_lyric.py
+++ scraping_lyric.py
@@ -1,20 +1,8 @@
 import requests
-from bs4 import BeautifulSoup
+import lxml.html
 
 artist_url = "https://www.uta-net.com/artist/12550/"
 artist_page = requests.get(artist_url)
 
-# htmlをBeautifulSoupで扱う
-soup = BeautifulSoup(artist_page.text, "html.parser")
-
-# タイトル要素を取得する → <title>経済、株価、ビジネス、政治のニュース:日経電子版</title>
-title_tag = soup.title
-
-# 要素の文字列を取得する → 経済、株価、ビジネス、政治のニュース:日経電子版
-title = title_tag.string
-
-# タイトル要素を出力
-print(title_tag)
-
-# タイトルを文字列を出力
-print(title)
+dom = lxml.html.fromstring(artist_page.text)
+print(dom.xpath('//*[@id="artist"]//table/tbody//td[@class="side td1"]/a[1]/@href'))
