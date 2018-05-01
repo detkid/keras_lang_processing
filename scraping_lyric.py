7b22f3dd0e6bcecc058edc6d0564a5add2da03ff
yutaro ohira <detkid@gmail.com>
2018-05-01 14:23:54 +0900

    乃木坂の歌詞全データ取得

--- scraping_lyric.py
+++ scraping_lyric.py
@@ -1,8 +1,25 @@
 import requests
 import lxml.html
 
+basic_url = "https://www.uta-net.com"
+
 artist_url = "https://www.uta-net.com/artist/12550/"
 artist_page = requests.get(artist_url)
 
+song_url_list = []
 dom = lxml.html.fromstring(artist_page.text)
-print(dom.xpath('//*[@id="artist"]//table/tbody//td[@class="side td1"]/a[1]/@href'))
+song_url_list = dom.xpath('//*[@id="artist"]//table/tbody//td[@class="side td1"]/a[1]/@href')
+
+lyrics = []
+
+for song in song_url_list:
+    lyric_url = basic_url + song
+    lyric_page = requests.get(lyric_url)
+    dom2 = lxml.html.fromstring(lyric_page.text) 
+    lyric = dom2.xpath("//*[@id='kashi_area']/text()")
+
+    lyrics.append(lyric)
+
+print('end')
+
+
