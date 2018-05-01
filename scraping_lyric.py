22ca4dabe6c022753f79fc2541b57812975fce22
yutaro ohira <detkid@gmail.com>
2018-05-01 15:05:54 +0900

    ファイル書き込みまで

--- scraping_lyric.py
+++ scraping_lyric.py
@@ -1,5 +1,6 @@
 import requests
 import lxml.html
+import pickle
 
 basic_url = "https://www.uta-net.com"
 
@@ -8,18 +9,27 @@ artist_page = requests.get(artist_url)
 
 song_url_list = []
 dom = lxml.html.fromstring(artist_page.text)
-song_url_list = dom.xpath('//*[@id="artist"]//table/tbody//td[@class="side td1"]/a[1]/@href')
+song_url_list = dom.xpath(
+    '//*[@id="artist"]//table/tbody//td[@class="side td1"]/a[1]/@href')
 
 lyrics = []
 
 for song in song_url_list:
     lyric_url = basic_url + song
     lyric_page = requests.get(lyric_url)
-    dom2 = lxml.html.fromstring(lyric_page.text) 
+    dom2 = lxml.html.fromstring(lyric_page.text)
     lyric = dom2.xpath("//*[@id='kashi_area']/text()")
 
-    lyrics.append(lyric)
+    song_lyric = ''
+    for line in lyric:
+        song_lyric = song_lyric + line
+    
+    lyrics.append(song_lyric)
 
-print('end')
+f = open('nogizaka_lyric.txt', 'wb')
 
+pickle.dump(lyrics, f)
 
+# f = open('nogizaka_lyric.txt', 'rb')
+
+# print(pickle.load(f))
